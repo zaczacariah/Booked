fdfe64f09b2680dbdb1fbadd42706bf28d7e0aec
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-05-20 19:15:57 +0930

    Connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://benzac:@Entity@hellbent123@booked.sogctvj.mongodb.net/');
 
 module.exports = mongoose.connection;
