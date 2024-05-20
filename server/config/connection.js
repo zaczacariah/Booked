06c42e682721da53accbab5577f9affc0de82ac9
zaczacariah <150572949+zaczacariah@users.noreply.github.com>
2024-05-20 20:22:31 +0930

    C

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,4 +2,20 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
+
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log(`Successfully connected to ${connectionString}`);
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+  });
+
+
 module.exports = mongoose.connection;
